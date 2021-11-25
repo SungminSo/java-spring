e85182b25fe9c9b9418be0ed26cb0224c1c1db46
SungminSo <soos196@hanyang.ac.kr>
2021-11-25 23:15:28 +0900

    chapter1-1 hello java

---
+++ Chapter1/src/ch01/HelloJava.java
@@ -0,0 +1,11 @@
+package ch01;
+// 패키지 이름은 숫자 들어가도 되고, 소문자로 하는게 일반적(컨벤션)
+
+// 클래스는 대문자로 시작하는게 일반적(컨벤션)
+// java 파일 하나에 클래스가 여러개 들어가는 경우 public class는 하나여야 함.
+public class HelloJava {
+
+    public static void main(String[] args) {
+	    System.out.println("Hello java");
+    }
+}
