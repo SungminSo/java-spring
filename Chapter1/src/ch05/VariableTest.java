88402194bc76e606095f26eb6bc2aa03cc0315a2
SungminSo <soos196@hanyang.ac.kr>
2021-11-27 11:53:02 +0900

    chapter1-6 integer

--- Chapter1/src/ch05/VariableTest.java
+++ Chapter1/src/ch05/VariableTest.java
@@ -10,5 +10,15 @@ public class VariableTest {
         // 4. 단, 변수 이름은 숫자로 시작하면 안됨.
         // 5. 이미 Java에서 예약어로 쓰이는것은 사용하면 안됨.
         // 변수 이름은 가독성이 가장 중요.
+
+        byte bnum = -128;  // (+)128일 경우에는 해당 타입의 숫자 범위가 넘어가서 에러
+        System.out.println(bnum);
+
+        // int num = 12345678900;  // Integer number too large
+        long lnum = 12345678900L;
+        // 만약 long 타입으로 선언했지만 뒤에 'L' 또는 'l'을 쓰지 않는다면 자동으로 int로 형변환 됨.
+        // long 타입 뒤에는 가급적이면 대문자로 적는게 가독성이 좋음
+        // 만약 long 타입보다 더 큰 범위의 숫자를 써야한다면? -> BigInteger 사용
+        // https://coding-factory.tistory.com/604
     }
 }
